7a5374973641963cfc21dc4cf4cf83a1331b4dd2
Nick Perez <nperez0111@gmail.com>
2015-11-19 20:32:56 -0500

    added features to Teleprompter scripts

--- admin/app/scripts/tele.js
+++ admin/app/scripts/tele.js
@@ -1,32 +1,19 @@
-var table;
+var tele;
 $.ajax({
     url: "http://localhost:9000/views/table.html",
     dataType: "html"
 }).then(function (template) {
-    table = new Table({
+    tele = new Tele({
         // The `el` option can be a node, an ID, or a CSS selector.
         el: '#container',
         template: template,
         // Here, we're passing in some initial data
         data: {
-            rows: ['Some', 'Error', 'Occurred'],
-            add: [],
-            editing: {
-                cur: -1,
-                past: {},
-                notAllowed: [false, false, false]
-            },
-            data: [
-            ["Check", "If", "Connected"],
-            ["To", "The", "Internet"]
-        ],
-            table: "users",
-            tables: ["users", "other", "MeantToCauseAlert"]
         }
     });
 
 }, function (err) {
-    table=new Table();
-    table.alert("Sorry, Issues loading template file...");
+    tele=new Tele();
+    tele.alert("Sorry, Issues loading template file...");
     return Error(err);
 });
