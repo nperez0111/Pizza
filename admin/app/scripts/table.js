5103f52a487199b8f7f978d8de71a93bc2334388
Nick Perez <nperez0111@gmail.com>
2015-12-04 23:56:41 -0500

    to fix the build

--- admin/app/scripts/table.js
+++ admin/app/scripts/table.js
@@ -123,7 +123,7 @@ var Table = Ractive.extend({
             this.sendToDataBase({
                 type: "POST",
                 data: arr,
-                url: "http://localhost:80/pizza/api/v1/"
+                url: "http://localhost:80/pizza/api/v1/" + this.get("table") + "/"
             });
         } else {
             //are the same do nothing
