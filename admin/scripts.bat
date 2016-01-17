d3df2f244f947fe19ccdd761e7e1f36098946f12
Nick Perez <nperez0111@gmail.com>
2016-01-17 11:07:41 -0500

    Added GitKraken to the flow

--- admin/scripts.bat
+++ admin/scripts.bat
@@ -2,4 +2,5 @@ git config --global credential.helper wincred
 call b
 call subl.exe
 call D:\Nick's\Desktop\pianobar.exe -new_console
+call C:\Users\npere_000\AppData\Local\gitkraken\Update.exe --processStart gitkraken.exe
 call run
