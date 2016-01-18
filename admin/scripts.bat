47ff1fbe6b693bd20832f30374541c4b0f7233fd
Nick Perez <nperez0111@gmail.com>
2016-01-18 09:47:07 -0500

    Bug fix startup tasks

--- admin/scripts.bat
+++ admin/scripts.bat
@@ -3,5 +3,5 @@ call b
 call subl.exe
 call D:\Nick's\Desktop\pianobar.exe -new_console
 call C:\Users\npere_000\AppData\Local\gitkraken\Update.exe --processStart gitkraken.exe
-call run
 start chrome "https://waffle.io/nperez0111/Pizza" "https://github.com/nperez0111"
+call run
