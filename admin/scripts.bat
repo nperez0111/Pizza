1a4b88af8047b058449441c69fc7bf7092a52521
Nick Perez <nperez0111@gmail.com>
2016-01-20 20:47:39 -0500

    Update Location of Nativefier

--- admin/scripts.bat
+++ admin/scripts.bat
@@ -3,6 +3,6 @@ call b
 call subl.exe
 call D:\Nick's\Desktop\pianobar.exe -new_console
 call C:\Users\npere_000\AppData\Local\gitkraken\Update.exe --processStart gitkraken.exe
-call C:\Users\npere_000\AppData\Local\Temp\electron-packager\win32-x64\nperez0111\Pizza-win32-x64\WaffleIO.exe
+call C:\Program Files\Pizza-win32-x64\WaffleIO.exe
 start chrome "https://github.com/nperez0111/Pizza"
 call run
