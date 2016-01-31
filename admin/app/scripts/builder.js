nperez0111/Pizza

commit b40ba98fbd400b5923f1005a9e4f57c0dc7737d1

Update builder.js

admin/app/scripts/builder.js
@@ -63,14 +63,14 @@ var Builder = Base.extend( {
                 types = [
                     []
                 ];
-            obj.forEach( function ( obj, i, arr ) {
+            obj.forEach( function ( obj ) {
                 if ( titles.indexOf( obj.Title ) === -1 ) {
                     if ( obj.Title === "Size" ) {
                         titles.unshift( obj.Title );
                         types.unshift( [ obj.Name ] );
                     } else {
                         titles.push( obj.Title );
-                        types[ titles.length - 1 ] = [ ( obj.Name ) ];
+                        types.push( [ ( obj.Name ) ] );
                     }
                 } else {
                     types[ titles.indexOf( obj.Title ) ].push( obj.Name );

commit 75e5ba147cd685dcdf6ba0261bf4750c62d941c2

Fixes bug introduced in b40ba98fbd400b5923f1005a9e4f57c0dc7737d1

admin/app/scripts/builder.js
@@ -70,7 +70,7 @@ var Builder = Base.extend( {
                         types.unshift( [ obj.Name ] );
                     } else {
                         titles.push( obj.Title );
-                        types.push( [ ( obj.Name ) ] );
+                        types[ titles.length - 1 ] = [ ( obj.Name ) ];
                     }
                 } else {
                     types[ titles.indexOf( obj.Title ) ].push( obj.Name );

admin/scripts.bat
@@ -3,6 +3,5 @@ call b
 call subl.exe
 call D:\Nick's\Desktop\pianobar.exe -new_console
 call C:\Users\npere_000\AppData\Local\gitkraken\Update.exe --processStart gitkraken.exe
-start "C:\Program Files\Pizza-win32-x64\WaffleIO.exe"
 start chrome "https://github.com/nperez0111/Pizza"
 call run