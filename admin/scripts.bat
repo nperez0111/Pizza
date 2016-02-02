d6c45958c69108f76c2ca09ed1f6b4ff51a87103
Nick Perez <nperez0111@gmail.com>
2016-02-01 20:01:36 -0500

    update script and gitignore of script

--- admin/scripts.bat
+++ admin/scripts.bat
@@ -1,7 +1,7 @@
 git config --global credential.helper wincred
 call b
 call subl.exe
-call D:\Nick's\Desktop\pianobar.exe -new_console
+call pianobar -new_console
 call C:\Users\npere_000\AppData\Local\gitkraken\Update.exe --processStart gitkraken.exe
 start chrome "https://github.com/nperez0111/Pizza"
 call run
