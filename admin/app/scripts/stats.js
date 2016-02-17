1edc7e253304bc91a1e18879a5fd82148c155ec6
Nick Perez <nperez0111@gmail.com>
2016-02-16 23:22:42 -0500

    added use case of getbytime

--- admin/app/scripts/stats.js
+++ admin/app/scripts/stats.js
@@ -1,10 +1,11 @@
-var Stats = Base.extend({
-    oninit: function() {
+var Stats = Base.extend( {
+    oninit: function () {
 
     },
-    data: function() {
-        return {
-            data: "well ok."
+    data: function () {
+            return {
+                data: "well ok."
+            }
         }
-    }
-});
+        //tele.sendToDataBase({data:{from:"2015-12-14 17:16:51"}},"getByTime/orders/DateOrdered").then(tele.logger,tele.logger)
+} );
