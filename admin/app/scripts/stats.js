3417d9ff63b295ce7af4fe6c98011665ad0cfd2d
Nick Perez <nperez0111@gmail.com>
2016-02-23 21:38:14 -0500

    Playing with stats and mustaches

--- admin/app/scripts/stats.js
+++ admin/app/scripts/stats.js
@@ -1,6 +1,12 @@
 var Stats = Base.extend( {
     oninit: function () {
-
+        tele.sendToDataBase( {
+            data: {
+                from: "2015-12-14 17:16:51"
+            }
+        }, "getByTime/orders/DateOrdered" ).then( JSON.parse, this.errorMessage ).then( ( resp ) => {
+            this.set( "data", resp );
+        } );
     },
     data: function () {
             return {
