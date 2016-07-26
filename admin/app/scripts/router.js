0147cbe8962b95370592548aba612068cccc6723
Nick The Sick <nperez0111@gmail.com>
2016-07-26 18:35:16 -0400

    :lipstick: changes

--- admin/app/scripts/router.js
+++ admin/app/scripts/router.js
@@ -16,44 +16,65 @@ module.exports = {
             Router.routes[ cur ] = null;
 
             page( cur, function () {
+
                 let handleattachements = () => {
+
                         if ( Router.routes[ cur ] === null ) {
+
                             return Promise.resolve( events.willAttach.call( this, cur + " has not been instantiated yet." ) ).then( ( a => {
+
                                 Router.routes[ cur ] = newRoutes[ cur ].apply( this, arguments );
+
                                 return true;
+
                             } )() ).then( events.hasAttached.apply( this, Router.routes[ cur ] ) );
 
                         } else {
+
                             return Promise.resolve( events.willAttach.call( this, Router.routes[ cur ] ) ).then( ( a => {
+
                                 Router.routes[ cur ].insert( Router.routes[ cur ].el );
+
                                 return true;
+
                             } )() ).then( events.hasAttached.call( this, Router.routes[ cur ] ) );
 
+
                         }
+
                     },
+
                     allAtOnce = () => {
+
                         return handleattachements.apply( this, arguments ).then( () => {
+
                             Router.cur = Router.routes[ cur ];
                             return Router.cur;
                         } );
                     };
-                console.log( Router.cur );
+
+
                 if ( Router.cur !== null ) {
+
                     return Promise.resolve( events.willDetach.call( this, Router.cur ) ).then( ( a => {
-                        console.log( Router.cur )
+
                         Router.cur.detach();
+
                         return true;
+
                     } ) ).then( events.hasDetached.call( this, Router.cur ) ).then( allAtOnce );
+
                 } else {
+
                     return allAtOnce();
+
                 }
 
 
-
-
-
             } );
+
         } );
+
     },
     base: function ( route ) {
         page.base( '/' );
